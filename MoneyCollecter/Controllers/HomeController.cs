4ad2169bd1f3029becd3de986c962e03c80875f6
Kevin Hicks <keyholesoftware@gmail.com>
2012-09-30 03:19:13 -0400

    Initial Commit
    Implemented First Page

---
+++ MoneyCollecter/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MoneyCollecter.Models;
+
+namespace MoneyCollecter.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Reciept reciept)
+        {
+            if (ModelState.IsValid)
+            {
+                CollectorDB db = new CollectorDB();
+                db.Reciepts.Add(reciept);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return Index();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MoneyCollecter/Models/RecieptModels.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MoneyCollecter.Models
+{
+    public class CollectorDB : DbContext
+    {
+        public CollectorDB()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<Reciept> Reciepts { get; set; }
+    }
+
+    public class Reciept
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [DataType(DataType.Currency)]
+        public float? WorldWideWork { get; set; }
+
+        [DataType(DataType.Currency)]
+        public float? KingdomHallFund { get; set; }
+
+        [DataType(DataType.Currency)]
+        public float? Congregation { get; set; }
+
+        [DataType(DataType.Currency)]
+        public float? Misc1 { get; set; }
+        public string Misc1Name { get; set; }
+
+        [DataType(DataType.Currency)]
+        public float? Misc2 { get; set; }
+        public string Misc2Name { get; set; }
+        
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+    }
+}
